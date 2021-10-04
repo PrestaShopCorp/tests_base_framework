61a9ce2ca8ab856b97fe5f4c8306d0a3d8cfd308
Boubker Bribri <boubker.bribri@prestashop.com>
2021-10-04 17:03:07 +0200

    Add custom query function

--- helpers/dbHelper.js
+++ helpers/dbHelper.js
@@ -35,6 +35,33 @@ class DbHelper {
   }
 
   /**
+   * Create a custom 'SELECT' query
+   * @param table {string} Name of the table
+   * @param fields {string|Array<string>} Fields to add to the request
+   * @return {Promise<string>}
+   */
+  async createCustomSelectQuery(table, fields = '*') {
+    const query = customFields => `SELECT ${customFields} FROM ${table};`;
+
+    if (typeof fields === 'string') {
+      return query(fields);
+    }
+
+    return query(fields.join(','));
+  }
+
+  /**
+   * Execute a custom 'SELECT' query
+   * @param table {string} Name of the table
+   * @param fields {string|Array<string>} Fields to add to the request
+   * @return {Promise<Array<Object>>}
+   */
+  async getResultsCustomSelectQuery(table, fields = '*') {
+    return this.getQueryResults(await this.createCustomSelectQuery(table, fields));
+  }
+
+
+  /**
    * Get query fields
    * @param query {String} Query to execute
    * @returns {Promise<Array<Object>>}
@@ -44,6 +71,10 @@ class DbHelper {
   }
 
 
+  /**
+   * Destroy sql connection
+   * @return {Promise<void>}
+   */
   async destroyConnection() {
     await this.connection.end();
   }
