7de2a856f0711f8ce22afea59715f5a2c40e54e0
Philippe Huet <philippe.huet@prestashop.com>
2022-07-07 10:41:15 +0200

    fix: remove ancient tree and not included types

--- types/examples/pages/mobile/firstVisit.d.ts
+++
@@ -1,8 +0,0 @@
-import 'module-alias/register';
-import { CommonPage } from '../../../src/utils/pages/commonPage';
-declare class FirstVisit extends CommonPage {
-    pageFrTitle: string;
-    constructor();
-}
-declare const firstVisit: FirstVisit;
-export { firstVisit };
