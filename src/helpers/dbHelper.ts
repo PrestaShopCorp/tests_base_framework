140caa37d522cfe374c40d80743401e22292ea3e
Philippe Huet <philippe.huet@prestashop.com>
2022-07-13 12:57:03 +0200

    fix: add types to function returns

--- src/helpers/dbHelper.ts
+++ src/helpers/dbHelper.ts
@@ -1,15 +1,22 @@
-import type {FieldPacket, Pool} from 'mysql2/promise';
 import {createPool} from 'mysql2/promise';
+
+import type {
+  FieldPacket,
+  Pool,
+  RowDataPacket,
+  OkPacket,
+  ResultSetHeader
+} from 'mysql2/promise';
 
 import {GlobalVars} from './globalVars';
 
 class DbHelper {
-  // functions
   /**
    * Create a pool
    * @param db
+   * @returns {Pool}
    */
-  createPool(db = GlobalVars.db) {
+  createPool(db = GlobalVars.db): Pool {
     return createPool(db);
   }
 
@@ -18,7 +25,20 @@ class DbHelper {
    * @param query {string} Query to execute
    * @returns {Query}
    */
-  async executeQuery(query: string) {
+  async executeQuery(
+    query: string
+  ): Promise<
+    [
+      (
+        | RowDataPacket[]
+        | RowDataPacket[][]
+        | OkPacket
+        | OkPacket[]
+        | ResultSetHeader
+      ),
+      FieldPacket[]
+    ]
+  > {
     const connection = this.createPool();
     const results = await connection.execute(query);
     await this.destroyConnection(connection);
@@ -30,7 +50,15 @@ class DbHelper {
    * @param query {string} Query to execute
    * @returns {Promise<Array<Object>>}
    */
-  async getQueryResults(query: string) {
+  async getQueryResults(
+    query: string
+  ): Promise<
+    | RowDataPacket[]
+    | RowDataPacket[][]
+    | OkPacket
+    | OkPacket[]
+    | ResultSetHeader
+  > {
     return (await this.executeQuery(query))[0];
   }
 
@@ -69,7 +97,13 @@ class DbHelper {
     table: string,
     fields: string | string[] = '*',
     conditions?: string
-  ) {
+  ): Promise<
+    | RowDataPacket[]
+    | RowDataPacket[][]
+    | OkPacket
+    | OkPacket[]
+    | ResultSetHeader
+  > {
     return this.getQueryResults(
       this.createCustomSelectQuery(table, fields, conditions)
     );
